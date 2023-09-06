2fc440ce9cee4cea4f1b1eeaffb91b631c7e6ad8
Pratham Reet <prathamreet@gmail.com>
2023-09-07 00:02:31 +0530

    added links

--- javascript/index.js
+++ javascript/index.js
@@ -16,11 +16,3 @@ navlink3.onclick = () => {
 }
 
 //=======================
-
-let bulba = document.getElementById("bulba")
-let trigger = document.getElementById("trigger")
-
-trigger.onclick = () => {
-    bulba.classList.toggle("bulbGlow");
-}
-
--- javascript/joke.js
+++ javascript/joke.js
@@ -1,3 +1,23 @@
+//==========navigation bar
+
+let navlink1 = document.getElementById("navlink1")
+navlink1.onclick = () => {
+    window.location = "./index.html"
+}
+
+let navlink2 = document.getElementById("navlink2")
+navlink2.onclick = () => {
+    window.location = "./joke.html"
+}
+
+let navlink3 = document.getElementById("navlink3")
+navlink3.onclick = () => {
+    window.location = "./light.html"
+}
+
+//=======================
+
+
 let Jokes = [
     
     `I just got my doctor's test results and I'm really upset about it. Turns out, I'm not gonna be a doctor.`,
